3eda3ea16b4b0a8492240a9a0f81104604375bbe
Semeton <balogunsemeton@gmail.com>
2022-12-05 23:12:19 +0100

    Testing performace

--- PHPArray/SplFixedArray.php
+++ PHPArray/SplFixedArray.php
@@ -3,3 +3,20 @@
  * @author Semeton Balogun <balogunsemeton@gmail.com>
  * SplFixedArray Method
  */
+$array = new SplFixedArray(10);
+for ($i=0; $i < 10; $i++) { 
+    $array[$i] = $i;
+}
+
+for ($i=0; $i < 10; $i++) { 
+    echo $array[$i]."<br/>";
+}
+
+/**
+ * Checking for performance
+ */
+
+ $startMemory = memory_get_usage();
+ $arr = range(1, 100000);
+ $endMemory = memory_get_usage();
+ echo ($endMemory - $startMemory)." bytes";
