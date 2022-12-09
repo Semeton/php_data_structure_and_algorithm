8b105abd9f674d3788007e5d1db2fdc78822d977
Semeton <balogunsemeton@gmail.com>
2022-12-09 11:57:40 +0100

    Linked list

---
+++ LinkedList/ListNode.php
@@ -0,0 +1,16 @@
+<?php
+
+ class ListNode {
+
+    /**
+     * @author Semeton Balogun <balogunsemeton@gmail.com>
+     */
+    
+    public $data = NULL;
+    public $next = NULL;
+    
+    public function __construct(string $data = Null)
+    {
+        $this->data = $data;
+    }
+ }
